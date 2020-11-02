725d0e23ec513270c6d02ff5f44438b16b24a051
LeonPJ <leon19941214@gmail.com>
2020-11-03 02:52:46 +0800

    home apge alter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 //import { Button } from 'semantic-ui-react';
 //import 'semantic-ui-css/semantic.min.css';
+import React, { useState } from 'react';
 import { 
   Button,
   Container,
@@ -10,12 +11,16 @@ import {
   Navbar,
   Nav,
   Form,
-  FormControl
+  FormControl,
+  Alert,
+  Heading,
+  Link
  } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Container fluid="true">
+      <AlertDismissible/>
       <>
         <Navbar bg="light" variant="light">
           <Navbar.Brand href="#home">Home</Navbar.Brand>
@@ -26,6 +31,29 @@ function App() {
         </Navbar>
       </>
     </Container>
+  );
+}
+
+function AlertDismissible() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      <Alert show={show} variant="success">
+        <Alert.Heading>How's it going?!</Alert.Heading>
+        <p>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+          lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+          fermentum.
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShow(false)} variant="outline-success">
+            Close me y'all!
+          </Button>
+        </div>
+      </Alert>
+    </>
   );
 }
 
