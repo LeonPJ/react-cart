d1cafe64133ee83541867ab9aa7f95fe2c2b15e4
LeonPJ <leon19941214@gmail.com>
2020-12-07 21:49:42 +0800

    LocalStorage

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -1,9 +1,15 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 const CartContext = createContext();
 
 const CartProvider = (props) => {
-    const [cart, setCart] = useState([]);
+
+    const LocalStorage = JSON.parse(localStorage.getItem('cart'));
+    const [cart, setCart] = useState(LocalStorage);
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))// localStorage.setItem(key, value)
+    }, [cart]);
+
     return (
         <CartContext.Provider value={[cart, setCart]}>
             {props.children}
