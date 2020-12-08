8cf685461caa882e5806bb3e4f5c2f33861a3039
LeonPJ <leon19941214@gmail.com>
2020-12-08 20:08:42 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
-import Alter from './components/alter';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Hint from './components/hint';
 import Menu from './components/menu';
-//import ProductList from './components/productlist';
-//import Cart from './components/cart';
-//import CartList from './components/cartlist';
 import { CartProvider } from './components/CartContext';
 import Section from './components/section';
-
-/*const App = () => {
-  return (
-    <BrowserRouter>
-      <h1>Hello user</h1>
-      <CartProvider>
-        <Menu />
-        <Section />
-      </CartProvider>
-    </BrowserRouter>
-  );
-}*/
+//import { Alert } from 'react-bootstrap';// console
 
 const App = () => {
   return (
     <CartProvider>
       <Router>
+        <Hint />
         <div className="app">
           <Menu />
           <Section />
